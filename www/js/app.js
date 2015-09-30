c82d4410cfb9b9d45e87c40b557774fa573f956a
famousvirus <argo@famousvirus.com>
2015-09-30 17:36:33 +0200

    add extra header hide

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('ideaalCatering', ['ionic', 'ionizer-wooshop.controllers', 'ionizer-wooshop.services', 'common.services'])
+angular.module('ideaalCatering', ['ionic', 'ionizer-wooshop.controllers', 'ionizer-wooshop.services', 'common.services','jett.ionic.scroll.sista'])
 
 .run(function($ionicPlatform) {
     //localStorage.clear();
